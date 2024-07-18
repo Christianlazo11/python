6f7c4fea9ccf2f994075d6b9c8de5a2efb582769
Christian Lazo <=>
2024-07-17 20:47:34 -0500

    Add Op_mate

---
+++ operaciones_matematicas.py
@@ -0,0 +1,21 @@
+
+a = 10
+b = 3
+
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+print(a == b)
+
+operacion_1 = 2 + 3 * 4
+print(operacion_1)
+
+operacion_2 = 2 + (3 * 4)
+print(operacion_2)
+
+operacion_3 = (2 + 3) * 4;
+print(operacion_3)
+
+operacion_4 = (2 + 3) * (4**2) / 8 -1
+print(operacion_4)
