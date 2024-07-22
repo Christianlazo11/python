7fc6c5869e44beca3ab086cc376e0698970e45e4
Christian Lazo <=>
2024-07-22 18:45:46 -0500

    Add op_arit

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,26 @@
+#Sumas y restas ( + y -)
+suma = 2024 + 2000
+print(suma)
+resta = 2024 - 2000
+print(resta)
+
+#Multiplicación y división
+multiplicacion = 23 * 7
+print(multiplicacion)
+division_decimal = 8 / 2
+print(division_decimal)
+
+#Potenciación
+potencia = 2 ** 3
+print(potencia)
+
+#División enter (//)
+division_entera = 12 // 4
+print(division_entera)
+
+# Resto(%)
+resto = 12 % 4;
+print(resto)
+
+tipo_de_dato = type(division_entera)
+print(tipo_de_dato)
